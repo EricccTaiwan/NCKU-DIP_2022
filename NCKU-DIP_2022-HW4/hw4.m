277392df3ea61c975d9b12b9b998978a2f4a5061
Eric Chou <87915276+EricccTaiwan@users.noreply.github.com>
2024-04-29 16:35:02 +0800

    HW4

---
+++ NCKU-DIP_2022-HW4/hw4.m
@@ -0,0 +1,4 @@
+clc;clear all;close all
+x = [1 4 7 4 3 6];
+x9_near = imresize(x,9/6,"nearest")
+
---
+++ NCKU-DIP_2022-HW4/hw4_four.m
@@ -0,0 +1,9 @@
+clc;clear all
+a = [ 2 3 4 5];
+a_f=fft(a')
+b = [2 -3 4 -5];
+b_f=fft(b')
+c= [ -9 -8 -7 -6];
+c_f=fft(c')
+d=[-9 8 -7 6];
+d_f=fft(d')
